4296a9117d59651b88c094155b4fa4ff17ded369
Tomáš Šmerda <tomas.smerda@cleevio.com>
2025-01-18 09:56:41 +0100

    Add assets

---
+++ RickAndMorty/Sources/Helpers/TextStyle.swift
@@ -0,0 +1,8 @@
+//
+//  TextStyle.swift
+//  RickAndMorty
+//
+//  Created by Tomáš Šmerda on 17.01.2025.
+//
+
+import Foundation
---
+++ RickAndMorty/Sources/Views/CharacterCardView.swift
@@ -0,0 +1,18 @@
+//
+//  CharacterCardView.swift
+//  RickAndMorty
+//
+//  Created by Tomáš Šmerda on 17.01.2025.
+//
+
+import SwiftUI
+
+struct CharacterCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharacterCardView()
+}
